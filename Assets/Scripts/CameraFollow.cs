76d998b63c250ad283b16c2495c34f0d20a21b08
Jaakko Lipsanen <jaakko.lipsanen@reaktor.fi>
2019-01-26 15:36:19 +0200

    Add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject ToFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ToFollow == null)
+        {
+            return;
+        }
+
+        Vector2 old = new Vector2(this.transform.position.x, this.transform.position.y);
+        Vector2 newPos = Vector2.Lerp(old, ToFollow.transform.position, Time.fixedDeltaTime * 5);
+        this.transform.position = new Vector3(newPos.x, newPos.y, this.transform.position.z);
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if(Input.GetKey(Forward))
         {
